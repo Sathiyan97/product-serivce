13d902ec52606eef674ee649ba7b67745bc74a70
Sathiyan <sathiyan97@gmail.com>
2023-06-18 10:32:08 +0530

    product controller & Repository added

---
+++ src/main/java/com/sathiyan/productserivce/service/ProductService.java
@@ -0,0 +1,47 @@
+package com.sathiyan.productserivce.service;
+
+import com.sathiyan.productserivce.dto.ProductRequest;
+import com.sathiyan.productserivce.dto.ProductResponse;
+import com.sathiyan.productserivce.model.Product;
+import com.sathiyan.productserivce.repository.ProductRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .Description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is saved.", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return products.stream().map(this::mapToProductResponse).toList();
+
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .Description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+
+}
